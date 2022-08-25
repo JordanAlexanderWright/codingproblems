60fd2e3a9e17228eb70057236329292c931c25b7
Jordan Wright <jaws610@gmail.com>
2022-08-25 11:13:50 -0400

    Messy.. but works.
    Iterated through each board until all were determined to be winners,
    then could return the last board and calc the score of that board.

--- day4/day_4_main.py
+++ day4/day_4_main.py
@@ -22,12 +22,40 @@ def play_bingo(bingo_data, chosen_numbers):
 
             # If there is a winner, return the table that is winning
             if winner:
-                print('Winner!', number)
+                # print('Winner!', number)
                 return table, table_number, number
             table_number += 1
 
+def last_winner_bingo(bingo_data, chosen_numbers):
+    # gets each board container
+
+    boards_that_won = []
+
+    for number in chosen_numbers:
+        print(number)
+        for table in bingo_data:
+            table_number = 1
+            for row in table:
+                for number_flag in row:
+                    if number_flag[0] == number:
+                        number_flag[1] = 1
+
+            print('checking', number, table_number)
+            winner = check_for_winner(table)
+
+            # If there is a winner, return the table that is winning
+            if winner:
+                if table in boards_that_won:
+                   pass
+                else:
+                    boards_that_won.append(table)
+                if len(bingo_data) == len(boards_that_won):
+
+                    return boards_that_won[-1], table_number, number
+
+            table_number += 1
+
 def check_for_winner(table):
-    print('checking')
     row = 0
     index = 0
 
@@ -40,6 +68,7 @@ def check_for_winner(table):
                 table[3][index][1] != 0 and \
                 table[4][index][1] != 0:
             print('won by column')
+            pprint(table)
             return True
         index += 1
         if index == 5:
@@ -92,19 +121,22 @@ def calc_score(table, last_number):
     for row in table:
         for number in row:
             if number[1] == 0:
-                print(number[0])
                 running_score += number[0]
 
     return (running_score * last_number)
 
 
 cleaned_data = clean_the_fucking_data(raw_data)
-pprint(cleaned_data)
-
 [winning_table, table_number, winning_number] = play_bingo(cleaned_data, selected_nums)
-
 winning_score = calc_score(winning_table, winning_number)
+#
+# pprint(winning_table)
+# print(f'Table Number: {table_number} wins with a score of {winning_score}')
 
-pprint(winning_table)
-print(f'Table Number: {table_number} wins with a score of {winning_score}')
+
+[last_table, last_table_num, last_num] = last_winner_bingo(cleaned_data, selected_nums)
+print('Last Table!', last_table, last_num)
+last_score = calc_score(last_table, last_num)
+#
+print(f'The last winner was: {last_table_num} with a score of {last_score}')
 
